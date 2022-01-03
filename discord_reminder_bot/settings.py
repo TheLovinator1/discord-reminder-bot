9ac6c995f8a16d01643108e0d1f74d90d9cdbdfe
Joakim Hellsén <tlovinator@gmail.com>
2022-01-03 15:10:06 +0100

    Add default value for bot_token

--- discord_reminder_bot/settings.py
+++ discord_reminder_bot/settings.py
@@ -8,9 +8,11 @@ from dotenv import load_dotenv
 load_dotenv(verbose=True)
 sqlite_location = os.getenv("SQLITE_LOCATION", default="/jobs.sqlite")
 config_timezone = os.getenv("TIMEZONE", default="Europe/Stockholm")
-bot_token = os.getenv("BOT_TOKEN")
+bot_token = os.getenv("BOT_TOKEN", default="")
 log_level = os.getenv(key="LOG_LEVEL", default="INFO")
 
+if not bot_token:
+    raise ValueError("Missing bot token")
 
 # Advanced Python Scheduler
 jobstores = {"default": SQLAlchemyJobStore(url=f"sqlite://{sqlite_location}")}
