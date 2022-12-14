8e01a84d0b2d92a8d9cbfab589efc6b3651d2a13
Joakim Hellsén <tlovinator@gmail.com>
2022-12-15 00:07:43 +0100

    Check if send_to_user is in main

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,9 @@ def test_if_send_to_discord_is_in_main():
     send_to_discords needs to be in main for this program to work.
     """
     assert hasattr(main, "send_to_discord")
+
+def test_if_send_to_user_is_in_main():
+    """
+    send_to_user needs to be in main for this program to work.
+    """
+    assert hasattr(main, "send_to_user")
